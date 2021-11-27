1bcef2820fc6a4893fad0f4f7546c55b68600d67
Caught Varyat <adam.dabrowski@outlook.com>
2021-11-27 20:56:08 +0100

    Get specific data points

--- camera-gear-favourite/main.py
+++ camera-gear-favourite/main.py
@@ -3,14 +3,25 @@ from os.path import isfile
 from PIL import Image, UnidentifiedImageError
 from PIL.ExifTags import TAGS
 
+specific = ['LensModel', 'FocalLength', 'Make', 'Model']
+result ={} 
+
 for item in listdir():
     if isfile(item):
         try:
             image = Image.open(item)
             for tag, value in image._getexif().items():
                 try:
-                    print(TAGS[tag], value)
-
+                    if TAGS[tag] in specific:
+                        result[TAGS[tag]] = {value: 1}
+                        print(TAGS[tag], value)
+                        print(result[TAGS[tag]][value])
+                        if result[TAGS[tag]][value] > 0:
+                            print('hello')
+                            result[TAGS[tag]][value] = result[TAGS[tag]][value] + 1
+                        else:
+                            print('e')
+                            result[TAGS[tag]] = {value: 1}
                 except(KeyError):
                     print('error')
         except(UnidentifiedImageError):
@@ -18,3 +29,4 @@ for item in listdir():
     else:
         print('directory')
         # TODO recursion on directory
+print(result)
